1822fbcfee9afd834386b9c19b4ceb8f6ec18668
fcogun <fuat.cogun@gmail.com>
2026-02-13 18:56:18 -0600

    Add rest of find functions

--- Project2/src/algorithm2.cpp
+++ Project2/src/algorithm2.cpp
@@ -44,4 +44,17 @@ void algorithm2()
 	{
 		std::cout << "Found a number in List! Position = " << std::distance(intList.begin(), it) << '\n';
 	}
+
+	it = std::find_first_of(intList.begin(), intList.end(), intVector.begin(), intVector.end(), [](int a, int b) {return a + b == 10; });
+	if (it != intList.end())
+	{
+		std::cout << "Found matching num in List! Position = " << std::distance(intList.begin(), it) << '\n';
+	}
+
+	it = std::adjacent_find(intList.begin(), intList.end(), [](int a, int b) { return a == 2 * b; });
+	if (it != intList.end())
+	{
+		std::cout << "Found adjacent values s.t. a=2b at position = " << std::distance(intList.begin(), it) << '\n';
+	}
+
 }
